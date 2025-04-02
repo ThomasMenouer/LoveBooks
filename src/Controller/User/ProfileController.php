55462ced6d32410be5c9b322255d8746803f0b9d
Thomas <tmenouer@gmail.com>
2025-04-02 18:24:43 +0200

    add profile controller

--- src/Controller/User/ProfileController.php
+++ src/Controller/User/ProfileController.php
@@ -5,7 +5,9 @@ namespace App\Controller\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_USER')]
 #[Route('/profile', name: 'profile_')]
 final class ProfileController extends AbstractController
 {
